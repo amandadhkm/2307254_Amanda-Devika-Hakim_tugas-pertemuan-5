57f5241a5a35d287777883f4d789f093f3677361
Amanda Devika Hakim <amandadhkm20@gmail.com>
2025-03-05 19:34:20 +0700

    Update SoalNo1.java

--- 2307254_AmandaDevikaHakim_Method/src/main/java/com/mycompany/_amandadevikahakim_method/SoalNo1.java
+++ 2307254_AmandaDevikaHakim_Method/src/main/java/com/mycompany/_amandadevikahakim_method/SoalNo1.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class SoalNo1 {
     
     public static void loopingText(int N){
-        for (int i = 1; i < N; i++){
+        for (int i = 0; i < N; i++){
             System.out.println("Saya senang belajar bahasa Java");
         }
     }
